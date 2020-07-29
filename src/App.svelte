151dbf3f721c8cdd9e3f06133e598984aae37e1e
John Cudd <jmcudd@gmail.com>
2020-07-29 18:17:35 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Level from "./Level.svelte";
+</script>
+
+<div>
+  <Level description="Level A" />
+  <Level description="Level B" highlighted />
+  <Level description="Level C" todo />
+  <Level description="Level D" todo />
+</div>
---
+++ src/Level.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let description;
+  export let highlighted;
+  export let todo;
+</script>
+
+<main>
+  <span class="level" class:highlighted class:todo>{description}</span>
+</main>
+
+<style>
+  .level {
+    margin-block-end: 1px;
+    opacity: 0.15;
+    padding: 0.5rem;
+    display: flex;
+  }
+  .level.highlighted {
+    border: 2px solid #00ff00;
+    opacity: 1;
+  }
+  .level.todo {
+    background: #eee;
+    opacity: 1;
+  }
+</style>
